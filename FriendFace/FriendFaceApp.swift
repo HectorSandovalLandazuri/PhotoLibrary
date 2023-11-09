08dc1d240d7adcc011c10bedb7ff40ecac94cd24
Héctor Sandoval <ingsan@live.com.mx>
2023-11-09 11:39:21 -0600

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
